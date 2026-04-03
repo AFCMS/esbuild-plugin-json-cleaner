31dada8cebb0fdf45fe30734f038338ac141bfe7
AFCMS <afcm.contact@gmail.com>
2026-04-03 23:03:12 +0200

    Explicit tsdown options

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
+  entry: "./src/index.ts",
+  format: "esm",
   dts: {
     sourcemap: true,
   },
   exports: true,
   sourcemap: true,
+  clean: true,
+  platform: "node",
   publint: {
     level: "suggestion",
   },
